3494f08670df831179d8d357e979d3b8cb7e9699
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2022-02-14 10:06:07 +0530

    changes in api url

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState("");
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=4ed9c1a5`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=4ed9c1a5`;
 
     await fetch(url)
       .then((response2) => {
